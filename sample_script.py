6ecdd0eeb17b2de84457e34e044657d34eb75c2a
JTHomas1784 <jamonte.thomas17@gmail.com>
2024-08-21 01:20:14 -0400

    Update automated search script

--- sample_script.py
+++ sample_script.py
@@ -18,7 +18,7 @@ driver.get('https://www.google.com/')
 # populate search field
 search = driver.find_element(By.NAME, 'q')
 search.clear()
-search.send_keys('MPC 3000')
+search.send_keys('MPC 3000 for sale')
 
 # wait for 4 sec
 sleep(4)
@@ -27,7 +27,7 @@ sleep(4)
 driver.find_element(By.NAME, 'btnK').click()
 
 # verify search results
-assert 'mpc 3000'.lower() in driver.current_url.lower(), f"Expected query not in {driver.current_url.lower()}"
+assert 'mpc 3000 for sale'.lower() in driver.current_url.lower(), f"Expected query not in {driver.current_url.lower()}"
 print('Test Passed')
 
 driver.quit()
